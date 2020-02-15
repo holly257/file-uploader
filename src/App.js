ef5c09c2405c92c5f891e2c28c029d05a55c1530
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-02-15 10:16:53 -0500

    added event handlers

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ class App extends React.Component {
     };
   }
 
+  updateSearchTerm(term) {
+    this.setState({
+      searchTerm: term
+    })
+  }
+
   render() {
     return (
       <div className='App'>
           <SearchBar 
             searchTerm={this.state.searchTerm}
             filterOption={this.state.filterOption}
+            handleUpdate={term=>this.updateSearchTerm(term)}
           />
           <FilterableList 
             files={this.props.files} 
